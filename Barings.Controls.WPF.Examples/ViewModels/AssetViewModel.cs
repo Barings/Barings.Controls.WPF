f450ba8731d33506fa5c463a27ce2b9de993db45
zbecknell <Zach.Becknell@Barings.com>
2017-02-08 09:08:05 -0500

    Changed some sample data

--- Barings.Controls.WPF.Examples/ViewModels/AssetViewModel.cs
+++ Barings.Controls.WPF.Examples/ViewModels/AssetViewModel.cs
@@ -14,11 +14,11 @@ namespace Barings.Controls.WPF.Examples.ViewModels
 		{
             Collection = new ObservableCollection<Asset>
             {
-		        new Asset { IssuerName = "Test Issuer 1", AssetName = "Test Asset 1", BabsonDefaultIndicator = "N", DefaultIndicator = true, MarketValue = 71.52m, Par = 100, MaturityDate = DateTime.Today.AddDays(-95), Price = 101.52m },
-                new Asset { IssuerName = "Test Issuer 1", AssetName = "Test Asset 2", BabsonDefaultIndicator = "Y", DefaultIndicator = true, MarketValue = 131.52m, Par = 100, MaturityDate = DateTime.Today, Price = 101.52m },
-                new Asset { IssuerName = "Test Issuer 2", AssetName = "Test Asset 3", BabsonDefaultIndicator = "N", DefaultIndicator = true, MarketValue = 101.52m, Par = 110, MaturityDate = DateTime.Today.AddDays(-29), Price = 101.52m },
-                new Asset { IssuerName = "Test Issuer 2", AssetName = "Test Asset 4", BabsonDefaultIndicator = "Y", DefaultIndicator = false, MarketValue = 121.52m, Par = 110, MaturityDate = DateTime.Today, Price = 101.52m },
-                new Asset { IssuerName = "Test Issuer 3", AssetName = "Test Asset 5", BabsonDefaultIndicator = "N", DefaultIndicator = false, MarketValue = 89.52m, Par = 100, MaturityDate = DateTime.Today, Price = 101.52m },
+		        new Asset { IssuerName = "Foo", AssetName = "Test Asset 1", BabsonDefaultIndicator = "N", DefaultIndicator = true, MarketValue = 71.52m, Par = 100, MaturityDate = DateTime.Today.AddDays(-95), Price = 101.52m },
+                new Asset { IssuerName = "Bar", AssetName = "Test Asset 2", BabsonDefaultIndicator = "Y", DefaultIndicator = true, MarketValue = 131.52m, Par = 100, MaturityDate = DateTime.Today, Price = 101.52m },
+                new Asset { IssuerName = "Snafu", AssetName = "Test Asset 3", BabsonDefaultIndicator = "N", DefaultIndicator = true, MarketValue = 101.52m, Par = 110, MaturityDate = DateTime.Today.AddDays(-29), Price = 101.52m },
+                new Asset { IssuerName = "FooBar Industries", AssetName = "Test Asset 4", BabsonDefaultIndicator = "Y", DefaultIndicator = false, MarketValue = 121.52m, Par = 110, MaturityDate = DateTime.Today, Price = 101.52m },
+                new Asset { IssuerName = "FUBAR", AssetName = "Test Asset 5", BabsonDefaultIndicator = "N", DefaultIndicator = false, MarketValue = 89.52m, Par = 100, MaturityDate = DateTime.Today, Price = 101.52m },
                 new Asset { IssuerName = "Bob Co.", AssetName = "Test Asset USD", BabsonDefaultIndicator = "N", DefaultIndicator = false, MarketValue = 101.52m, Par = 100, MaturityDate = DateTime.Today, Price = 101.52m },
                 new Asset { IssuerName = "Jim Bob Inc.", AssetName = "Test Asset EUR", BabsonDefaultIndicator = "N", DefaultIndicator = false, MarketValue = 101.52m, Par = 110, MaturityDate = DateTime.Today.AddDays(4), Price = 101.52m },
             };
